6bb83944bda857270e1488c25d87698dfcaa5627
luwenzheng <2311410554@qq.com>
2020-07-22 15:41:24 +0800

    测试git代码合并

--- src/main/java/com/example/democode/DemocodeApplication.java
+++ src/main/java/com/example/democode/DemocodeApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemocodeApplication {
 
+    /**
+     * test 代码分支合并
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(DemocodeApplication.class, args);
     }
